ea61d9949dcf6a7411d486132647642de51ca4ac
Tracy Karanja <chirutracy@gmail.com>
2024-05-25 19:07:29 +0300

    new commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page):
+      self.title = title
+      self.page = page
     
+    @property
+    def page_count(self):
+        """This is the page property"""
+        return self._page
+    
+    @page_count.setter
+    def page_count(self, page):
+        if not isinstance(page, int):
+            print("page_count must be an integer")
+        else:
+            self._page = page
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = 'Used'
+        
+    @property
+    def int_size(self):
+        return self._size
+    
+    @int_size.setter
+    def int_size(self, size):
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        else:
+            self._size = size
+            
+    def can_cobble():
+        print("Your shoe is as good as new!")
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
